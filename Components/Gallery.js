d159bb40c205d38f3b3641d566525d5f84fb7764
Rial <rialparmar007@gmail.com>
2024-10-01 12:43:20 -0700

    Changes in images

--- Components/Gallery.js
+++ Components/Gallery.js
@@ -8,8 +8,8 @@ const galleryItems = [
   { src: '/gallery-3.jpg', label: 'Painting Competition' },
   { src: '/gallery-4.jpg', label: 'Church Band' },
   { src: '/gallery-5.jpg', label: 'Church Choir' },
-    { src: '/gallery-6.jpg', label: 'Cycling' }
-  ];
+  { src: '/gallery-6.jpg', label: 'Cycling' }
+];
 
 const Gallery = () => {
   return (
@@ -18,7 +18,7 @@ const Gallery = () => {
       <div className={styles.gallery}>
         {galleryItems.map((item, index) => (
           <div key={index} className={`${styles.galleryItem} ${styles[`item${index + 1}`]}`}>
-            <img src={item.src} alt={item.label} className={styles.galleryImage} />
+            <img src={`${process.env.assetPrefix}${item.src}`} alt={item.label} className={styles.galleryImage} />
             <div className={styles.galleryLabel}>{item.label}</div>
           </div>
         ))}
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
+const isDev = process.env.NODE_ENV !== 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath:'/churchwebsite',
-    distDir:'dist',
-    output: 'export'
+    basePath: '/churchwebsite',
+    distDir: 'dist',
+    output: 'export',
+    assetPrefix: isDev ? undefined : 'https://rialparmar1777.github.io/churchwebsite',
+    env: {
+        assetPrefix: isDev ? '' : 'https://rialparmar1777.github.io/churchwebsite',
+    },
 };
 
 export default nextConfig;
