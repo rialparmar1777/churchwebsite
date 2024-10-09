2c7be07043539d04241c1d885f100e31cd645ee7
Rial <rialparmar007@gmail.com>
2024-10-09 11:00:10 -0700

    Fixing the link errors in the path.

--- app/About1/page.js
+++ app/About1/page.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '@/Components/Navbar'; // Adjust path as necessary
+import Footer from '@/Components/Footer'; // Adjust path as necessary
+import About1 from '@/app/about1/About1.js';
 
-const About1 = () => {
+const About1Page = () => {
   return (
     <div>
-      
+      <Navbar />
+      <About1 />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default About1
+export default About1Page;
